65180fcb1053e0af52a3449122869f6d61203179
Lucas <104462643+LucasPriestley@users.noreply.github.com>
2022-08-03 12:59:13 +0100

    updated crud opertions and routes

--- src/user/controllers.js
+++ src/user/controllers.js
@@ -1,24 +1,50 @@
+const jwt = require("jsonwebtoken");
 const User = require("./model");
 
 exports.createUser = async (req, res) => {
   try {
     const newUser = await User.create(req.body);
-    console.log(newUser);
-    res.send({ msg: "This came from createUser" });
+    const token = await jwt.sign({ _id: newUser._id }, process.env.SECRET); //create token with user._id inside
+    //generate token using newUser._id
+    res.send({ msg: "This came from createUser", token }); //send success message and token back in the response
   } catch (error) {
     console.log(error);
+    res.send({ err: error });
   }
 };
 
-exports.findUser = async (req, res) => {
+exports.login = async (req, res) => {
   try {
-    const findUser = await User.find(req.body);
-    console.log(findUser);
-    res.send({ msg: "This came from findUser" });
+    const token = await jwt.sign({ _id: req.user._id }, process.env.SECRET); //create token with user._id inside
+    res.send({ user: req.user.username, token });
   } catch (error) {
     console.log(error);
+    res.send({ err: error });
   }
 };
+
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find({});
+    const result = users.map((u) => {
+      return u.username;
+    });
+    res.send({ allUsers: result });
+  } catch (error) {
+    console.log(error);
+    res.send({ err: error });
+  }
+};
+
+// exports.findUser = async (req, res) => {
+//   try {
+//     const findUser = await User.find(req.body);
+//     console.log(findUser);
+//     res.send({ msg: "This came from findUser" });
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
 
 exports.updateUser = async (req, res) => {
   try {
@@ -42,3 +68,15 @@ exports.deleteUser = async (req, res) => {
     console.log(error);
   }
 };
+
+// exports.login = async (req, res) => {
+//   try {
+//     const user = await user.findOne({
+//       username: req.body.username,
+//       password: req.body.password,
+//     });
+//   } catch (error) {
+//     console.log(error);
+//     res.send({ err: error });
+//   }
+// };
--- src/user/routes.js
+++ src/user/routes.js
@@ -2,17 +2,40 @@ const { Router } = require("express");
 const userRouter = Router();
 const {
   createUser,
+  login,
+  getAllUsers,
   findUser,
   updateUser,
   deleteUser,
 } = require("./controllers");
+const { hashPass, comparePass, tokenCheck } = require("../middleware");
 
-userRouter.post("/user", createUser);
+userRouter.post("/user", hashPass, createUser);
 
-userRouter.get("/user", findUser);
+userRouter.post("/login", comparePass, login);
+
+userRouter.get("/user", getAllUsers);
+
+userRouter.get("/login", tokenCheck, login);
+
+// userRouter.get("/user", findUser);
 
 userRouter.patch("/user", updateUser);
 
 userRouter.delete("/user", deleteUser);
 
 module.exports = userRouter;
+
+// const loginFetch = async (username, pass) => {
+//     const res = await fetch("http://localhost:5001/login", {
+//         method: "POST",
+//         headers: {"Content-Type": "application/json"},
+//         body: JSON.stringify({
+//             username: username,
+//             password: pass
+//         })
+//     })
+//     const data = await res.json()
+//     data.user
+//     data.token
+// } This is for next week, only in React app
